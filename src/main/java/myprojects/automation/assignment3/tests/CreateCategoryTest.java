478da8f30752cd6d7fcc5eba88d1953c4e40531a
Max Kurii <kuriimaxym@gmail.com>
2018-06-12 17:12:02 +0300

    Initial commit

---
+++ src/main/java/myprojects/automation/assignment3/tests/CreateCategoryTest.java
@@ -0,0 +1,25 @@
+package myprojects.automation.assignment3.tests;
+
+import myprojects.automation.assignment3.BaseScript;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.sql.Driver;
+
+public class CreateCategoryTest extends BaseScript {
+    public static void main(String[] args) throws InterruptedException {
+
+        // TODO prepare driver object
+        // WebDriver driver = getConfiguredDriver();
+        // ...
+
+        // login
+
+        // create category
+
+        // check that new category appears in Categories table
+
+        // finish script
+    }
+}

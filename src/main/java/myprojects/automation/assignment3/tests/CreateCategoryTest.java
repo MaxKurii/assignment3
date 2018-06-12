ceb8a6b11eab50e9f786ecfee4af179a76c065e8
Max Kurii <kuriimaxym@gmail.com>
2018-06-12 17:34:47 +0300

    1

--- src/main/java/myprojects/automation/assignment3/tests/CreateCategoryTest.java
+++ src/main/java/myprojects/automation/assignment3/tests/CreateCategoryTest.java
@@ -1,11 +1,7 @@
 package myprojects.automation.assignment3.tests;
 
 import myprojects.automation.assignment3.BaseScript;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
-import java.sql.Driver;
 
 public class CreateCategoryTest extends BaseScript {
     public static void main(String[] args) throws InterruptedException {
